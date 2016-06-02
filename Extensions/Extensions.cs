222084fa6e3499e41ed6be9c2686ba59d976848e
Wouter Van Speybroeck <wouter.van.speybroeck@gmail.com>
2016-06-02 10:02:29 +0200

    Added a piped add to IList<>

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static IList<T> PipedAdd<T>(this IList<T> l, T item)
+        {
+            l.Add(item);
+            return l;
+        }
+    }
+}
